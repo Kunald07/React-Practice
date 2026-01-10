56d287dc679306085aadb80f2651d1f2f5f48d06
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-01-10 23:09:27 +0530

    Added 06-practice

---
+++ 06-practice/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" w-1/2 m-auto mt-10">
+      <h1 className="text-5xl text-red-200 mb-2">Home</h1>
+      <p className="mb-2 text-zinc-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        eveniet quisquam sit optio labore nemo alias quidem consequatur?
+        Suscipit deleniti incidunt voluptatibus cumque necessitatibus ex
+        quisquam iusto aspernatur harum officia?
+      </p>
+      <button className="p-3 text-white bg-zinc-300">Explore More</button>
+    </div>
+  );
+};
+
+export default Home;
